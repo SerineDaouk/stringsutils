50e30d40edce40756a5499d08cb93f69db9a8ec7
SerineDaouk <serine.aitechom@gmail.com>
2023-06-05 18:53:56 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SerineDaouk/stringsutils
+
+go 1.19
---
+++ stringsutils.go
@@ -0,0 +1,15 @@
+package stringsutils
+
+import "strings"
+
+func Upper(word string) string {
+
+	return strings.ToUpper(word)
+
+}
+
+func Lower(word string) string {
+
+	return strings.ToLower(word)
+
+}
